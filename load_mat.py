9a46811d0bef7d7091bef71e60c862c2713927a9
Evans Jahja <evans.jahja@yahoo.com>
2019-05-16 00:28:56 +0700

    Saves load_mat to h5

--- load_mat.py
+++ load_mat.py
@@ -1,7 +1,9 @@
 from scipy.io import loadmat
 import cv2
 import numpy as np
+import h5py
 trainMat = loadmat("train_32x32.mat")
+testMat = loadmat("test_32x32.mat")
 #print trainMat["X"][0]
 
 # Unsharp Mask
@@ -25,7 +27,9 @@ trainMat["X"] = trainMat["X"].transpose((3,0,1,2))
 
 image_count = len(trainMat["X"])
 
-newTrainMat = {"X":[], "Y": []}
+val_size = image_count // 10
+
+newTrainMat = {"X":[], "y": []}
 
 # Convert to B/W
 for i in range(image_count):
@@ -33,9 +37,11 @@ for i in range(image_count):
     num = trainMat["y"][i]
     if num == 10:
         num = 0
-    newTrainMat["Y"].append(num)
-
-
+    newTrainMat["y"].append(num)
+newTrainMat["y"] = np.array(newTrainMat["y"], dtype=np.uint8)
+newTestMat = {"X":[], "y": []}
+newTestMat["X"] = np.array(testMat["X"])
+newTestMat["y"] = np.array(testMat["y"])
 for i in range(image_count):
 
     # Equalize Hist
@@ -46,10 +52,21 @@ for i in range(image_count):
 
     newTrainMat["X"][i] = unsharp(newTrainMat["X"][i], 0.1)
 
+# Saves to .h5py
+f = h5py.File('SVHN_grey.h5', 'w')
 
 
+#newTrainMat["X"] = np.array(newTrainMat["X"])
+#f.create_dataset('X_train', newTrainMat["X"].shape, data=newTrainMat["X"])
+f.create_dataset('X_train', data=newTrainMat["X"])
+f.create_dataset('y_train', data=newTrainMat["y"])
+f.create_dataset('X_val', data=newTrainMat["X"][:val_size])
+f.create_dataset('y_val', data=newTrainMat["y"][:val_size])
+f.create_dataset('X_test', data=newTestMat["X"])
+f.create_dataset('y_test', data=newTestMat["y"])
+f.close()
 idx = 2
-print(newTrainMat["Y"][idx])
+print(newTrainMat["y"][idx])
 cv2.imshow("original", trainMat["X"][idx])
 
 cv2.imshow("processed", newTrainMat["X"][idx])
