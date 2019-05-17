e09b865b3f734f9501fc1fa68661f8a5db4ba99e
Edward H.M <edwardhm865@gmail.com>
2019-05-17 09:38:43 +0700

    Update fitur upload file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, jsonify, redirect
 from werkzeug import secure_filename
+import os
 from werkzeug.wrappers import response
 import json
 
 app = Flask(__name__)
+os.makedirs(os.path.join(app.instance_path, 'Data_Training'), exist_ok=True)
+os.makedirs(os.path.join(app.instance_path, 'Img_Predict'), exist_ok=True)
+
 @app.route("/")
 def home():
     return render_template('UI_UAS.html')
@@ -13,11 +17,18 @@ def predict():
     return render_template('prediksi.html')
 
 @app.route('/predict/preprocessing', methods=['POST'])
-def upload():
+def uploadImage():
     file = request.files['test']
-    filename = secure_filename(file.filename)
-    file.save(filename)
+     # filename = secure_filename(file.filename)
+    file.save(os.path.join(app.instance_path, 'Img_Predict', secure_filename(file.filename)))
     return '',204
+
+@app.route('/latih/training', methods=['POST'])
+def uploadData():
+     file = request.files['Latih']
+     # filename = secure_filename(file.filename)
+     file.save(os.path.join(app.instance_path, 'Data_Training', secure_filename(file.filename)))
+     return '',204
     
 if __name__ == "__main__":
     app.run(debug=True)
